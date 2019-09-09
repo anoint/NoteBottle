8482c870fb5f47ab3541551f6b68d91b62505860
keyvir <kimyu711@naver.com>
2019-09-09 18:56:43 +0900

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col>
+          {{$store.getters.isLogined}}
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="id"
+            :counter="10"
+            label="ID"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="password"
+            :counter="10"
+            label="PW"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn @click="login">
+            login
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      id: '',
+      password: ''
+    }),
+    methods: {
+      login () {
+        this.$store.dispatch('loginProcess',{id:this.id,password:this.password}).then(()=>{
+          console.log(this.$store.state.auth)
+        })
+      }
+    },
+    mounted () {
+      console.log(this.$store.state.auth)
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,61 @@ import router from './router'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-
 Vue.use(Vuetify)
 Vue.use(Vuex)
+
+const AuthModule = {
+  state: {
+    isLogined: true,
+    token: ''
+  },
+  getters: {
+    isLogined: state => {
+      return state.token!=='' && state.isLogined;
+    }
+  },
+  mutations: {
+    login (state) {
+      state.isLogined = true
+    },
+    logout (state) {
+      state.isLogined = false
+    },
+    saveToken (state, token) {
+      state.token = token
+    }
+  },
+  actions: {
+    loginProcess (context,data) {
+      return new Promise((resolve,reject)=>{
+        var xhr = new XMLHttpRequest();
+        xhr.open('post','http://notebottle.api.test/login',true);
+        xhr.onreadystatechange = ()=>{
+          if(xhr.readyState === 4 && xhr.status ===200){
+            context.commit('login')
+            context.commit('saveToken',xhr.responseText)
+          }
+        }
+        var params = 'id='+data.id+'&password='+data.password
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send(params)
+      })
+      context.commit('login')
+    }
+  }
+}
+
+const mainStore = new Vuex.Store({
+  modules: {
+    auth: AuthModule
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  Vuetify,
+  store:mainStore,
   render: h => h(App)
 }).$mount('#app')
