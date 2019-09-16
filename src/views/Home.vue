fa8bf3a4fc5303bbda2555d190ad25175e3074d4
anoint <dbwlsk870@hanmail.net>
2019-09-16 15:54:37 +0900

    0916

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,25 +12,12 @@
       <!-- Navigation drawers -->
       <v-card height="350px" class="font-white" style="z-index:1;">
         <v-navigation-drawer absolute permanent right>
-          <template v-slot:prepend>
-            <v-list-item two-line>
-              <v-list-item-avatar>
-                <img src="https://randomuser.me/api/portraits/women/81.jpg" />
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Jane Smith</v-list-item-title>
-                <v-list-item-subtitle>Logged In</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-
           <v-divider></v-divider>
 
           <v-list dense>
             <v-list-item v-for="item in items" :key="item.title">
               <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
+                <v-icon>{{ item.icon }}</v-icon>  
               </v-list-item-icon>
 
               <v-list-item-content>
@@ -45,6 +32,7 @@
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   data() {
     return {
